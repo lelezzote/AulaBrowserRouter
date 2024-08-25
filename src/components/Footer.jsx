cb321a8f379683c1c5e5be61eeac80c67ebc04ef
lelezzote <leticia.santos11@estudante.ifms.edu.br>
2024-08-25 16:19:39 -0300

    teste

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,17 +3,17 @@ export default function Footer(){
         <footer>
         <div className="footer-foot py-2">
         <img className="logo-footer" src="https://http2.mlstatic.com/D_NQ_NP_665142-MLB70516948364_072023-O.webp"/>
+        
         <ul className="navbar-footer">
                 <li>Nossas Lojas</li>
                 <li>Entrega e Frete</li>
                 <li>Trocas e Devoluções</li>
                 <li>Política de Segurança</li>
+                <li>Forma de pagamento</li>
+                <li>Política de Privacidade</li>
+                <li>Central de Ajuda</li>
             </ul>
-            <ul className="navbar-footer-secundary">
-                <li>Minha Conta</li>
-                <li>Meus Pedidos</li>
-                <li>Meu Carrinho</li>
-            </ul>
+            
             
             
             </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,6 +8,8 @@ export default function Header(){
     <nav className='navbar'>
     <div className="navbar-header">
     
+
+
         <ul className="navbar-menu">
             <Link to="/">
                <li className="navbar-li"> Home </li>
@@ -18,6 +20,12 @@ export default function Header(){
             <Link to="/ofertas">
                <li> Oferta </li>
             </Link>
+        </ul>
+        <ul className="navbar-second">
+            <img className="carrinho" src='https://cdn-icons-png.flaticon.com/128/34/34568.png'/>
+        </ul>
+        <ul>
+        <img className='login' src="https://cdn-icons-png.flaticon.com/128/1077/1077063.png"/>
         </ul>
     
     </div>
